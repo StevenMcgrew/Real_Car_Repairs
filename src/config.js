3e05b493266af5c61adb73394e4ed5f8a3194125
StevenMcgrew <stevenmcgrew8224@msn.com>
2023-02-02 22:30:10 -0800

    Updated config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const backendBaseUrl = 'http://localhost:3000';
+// export const backendBaseUrl = 'http://localhost:3000';
 
-// export const backendBaseUrl = 'https://stevenmcgrew.us';
+export const backendBaseUrl = 'https://stevenmcgrew.us';
 export const apiBaseUrl = backendBaseUrl + '/api/vehicle-repairs';
 export const imagesBaseUrl = backendBaseUrl + '/repair-images';
